58328dcf52378b73a3b6530f240a70df6b7ed119
Thiago Luiz de Matos <127130121+thidematos@users.noreply.github.com>
2023-10-10 03:02:02 -0300

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -15,7 +15,7 @@ const createSignToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    httpOnly: false,
   });
 
   res.status(statusCode).json({
